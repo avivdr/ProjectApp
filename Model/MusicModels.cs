0321af7af469bbc06716cc275d215f02b2c0e094
Aviv Dor <avivdor.ad@gmail.com>
2023-10-25 16:03:42 +0300

    open opus service & music models

---
+++ Model/MusicModels.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using ProjectApp.Services;
+
+namespace ProjectApp.Model
+{
+    public class Request
+    {
+        public string type { get; set; }
+        public string item { get; set; }
+        public string search { get; set; }
+        public string offset { get; set; }
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize(this, OpenOpusService.options);
+        }
+    }
+    public class Status
+    {
+        public string version { get; set; }
+        public string success { get; set; }
+        public string source { get; set; }
+        public int rows { get; set; }
+        public float processingtime { get; set; }
+        public string api { get; set; }
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize(this, OpenOpusService.options);
+        }
+    }
+
+    #region Objects
+    public class Work
+    {
+        public string title { get; set; }
+        public string subtitle { get; set; }
+        public string searchterms { get; set; }
+        public string popular { get; set; }
+        public string recommended { get; set; }
+        public string id { get; set; }
+        public string genre { get; set; }
+        public string searchmode { get; set; }
+        public string catalogue { get; set; }
+        public string catalogue_number { get; set; }
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize(this, OpenOpusService.options);
+        }
+
+    }
+    public class Composer
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string complete_name { get; set; }
+        public string birth { get; set; }
+        public string death { get; set; }
+        public string epoch { get; set; }
+        public string portrait { get; set; }
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize(this, OpenOpusService.options);
+        }
+    }
+    #endregion
+
+    #region Requests
+    public class ComposerResult
+    {
+        public Status status { get; set; }
+        public Request request { get; set; }
+        public List<Composer> composers { get; set; }
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize(this, OpenOpusService.options);
+        }
+    }
+
+    public class GenreResult
+    {
+        public Status status { get; set; }
+        public Composer composer { get; set; }
+        public List<string> genres { get; set; }
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize(this, OpenOpusService.options);
+        }
+    }
+
+    public class WorkResult
+    {
+        public Status status { get; set; }
+        public Request request { get; set; }
+        public Composer composer { get; set; }
+        public List<Work> works { get; set; }
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize(this, OpenOpusService.options);
+        }
+    }
+
+    public class OmniSearchResult
+    {
+        public Status status { get; set; }
+        public Request request { get; set; }
+        public List<OmniSearchItem> results { get; set; }
+        public int next { get; set; }
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize(this, OpenOpusService.options);
+        }
+    }
+
+    public class OmniSearchItem
+    {
+        public Composer composer { get; set; }
+        public Work work { get; set; }
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize(this, OpenOpusService.options);
+        }
+    }
+    #endregion    
+}
---
+++ Services/OpenOpusService.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using ProjectApp.Model;
+
+namespace ProjectApp.Services
+{
+    public enum Period
+    {
+        Medieval,
+        Renaissance,
+        Baroque,
+        Classical,
+        Early_Romantic,
+        Romantic,
+        Late_Romantic,
+        _20th_Century,
+        Post_War,
+        _21st_Century,
+    }
+
+    public enum Genre
+    {
+        Keyboard,
+        Chamber,
+        Orchestral,
+        Stage,
+        Vocal
+    }
+
+    public class OpenOpusService
+    {
+        private HttpClient client;
+        const string URL = @"https://api.openopus.org";
+        public static JsonSerializerOptions options = new JsonSerializerOptions()
+        {
+            WriteIndented = true
+        };
+
+        public OpenOpusService()
+        {
+            client = new HttpClient();
+        }
+
+        public List<string> Periods = new List<string>()
+        {
+            "Medieval",
+            "Renaissance",
+            "Baroque",
+            "Classical",
+            "Early Romantic",
+            "Romantic",
+            "Late Romantic",
+            "20th Century",
+            "Post-War",
+            "21st Century"
+        };
+
+        public List<string> Genres = new List<string>()
+        {
+            "Keyboard",
+            "Chamber",
+            "Orchestral",
+            "Stage",
+            "Vocal"
+        };
+        public static List<Work> FilterThing(List<Work> list)
+        {
+            return list.Where(work => work.genre != "Keyboard" &&
+            (work.title.ToLower().Contains("piano") || work.title.ToLower().Contains("keyboard"))).ToList();
+        }
+
+        public static List<Work> FilterKeyboard(List<Work> list)
+        {
+            return list.Where(work => work.genre == "Keyboard").ToList();
+        }
+
+        //useful functions
+
+        //for search
+        public async Task<OmniSearchResult> OmniSearch(string query)
+        {
+            try
+            {
+                var response = await client.GetAsync($"{URL}/omnisearch/{query}/0.json");
+                if (response.IsSuccessStatusCode)
+                {
+                    string content = await response.Content.ReadAsStringAsync();
+
+                    var result = JsonSerializer.Deserialize<OmniSearchResult>(content);
+                    if (result != null && result.status.success == "true")
+                        return result;
+                }
+            }
+            catch (Exception) { }
+            return null;
+        }
+
+        public async Task<WorkResult> WorksByComposerID(int composerID)
+        {
+            try
+            {
+                var response = await client.GetAsync($"{URL}/work/list/composer/{composerID}/genre/all.json");
+                if (response.IsSuccessStatusCode)
+                {
+                    string content = await response.Content.ReadAsStringAsync();
+                    var result = JsonSerializer.Deserialize<WorkResult>(content);
+                    if (result != null && result.status.success == "true")
+                        return result;
+                }
+            }
+            catch (Exception) { }
+            return null;
+        }
+
+        public async Task<ComposerResult> PopularComposers()
+        {
+            try
+            {
+                var response = await client.GetAsync($"{URL}/composer/list/pop.json");
+                if (response.IsSuccessStatusCode)
+                {
+                    string content = await response.Content.ReadAsStringAsync();
+                    return JsonSerializer.Deserialize<ComposerResult>(content);
+                }
+            }
+            catch (Exception) { }
+            return null;
+        }
+
+        public async Task<ComposerResult> ComposersInPeriod(Period period)
+        {
+            try
+            {
+                var response = await client.GetAsync($"{URL}/composer/list/epoch/{Periods[(int)period]}.json");
+                if (response.IsSuccessStatusCode)
+                {
+                    string content = await response.Content.ReadAsStringAsync();
+                    return JsonSerializer.Deserialize<ComposerResult>(content);
+                }
+            }
+            catch (Exception) { }
+            return null;
+        }
+    }
+}
